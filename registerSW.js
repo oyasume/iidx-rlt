03129dc2480c2914b3166539b9200ad0dd35e903
oyasume <oyasume@users.noreply.github.com>
2025-07-06 09:08:10 +0000

    deploy: e83436fa76dfbf4ae92a8afb5c28442ca1eba398

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/iidx-rlt/sw.js', { scope: '/iidx-rlt/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
