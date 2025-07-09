07cab2f098cb3a50c7b8ef67be025a6a0a41733e
iidx-rlt <215342013+oyasume@users.noreply.github.com>
2025-07-10 02:13:16 +0900

    fix: #1 ブックマークレットを表示の補足情報をAccordionにする

--- src/component/TicketImporter.tsx
+++ src/component/TicketImporter.tsx
@@ -12,7 +12,11 @@ import {
   Link,
   CircularProgress,
   TextField,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
 } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { Ticket } from "../types";
 import { ClipboardSnackbar } from "./ClipboardSnackBar";
@@ -76,23 +80,10 @@ export const TicketImporter: React.FC<TicketImporterProps> = ({ onImport }) => {
           </StepLabel>
           <StepContent>
             <Typography>IIDX公式サイトでブックマークレットを実行し、チケット情報をコピーします。</Typography>
-            <Link component="button" onClick={() => setShowBookmarklet(!showBookmarklet)} sx={{ textAlign: "left" }}>
+            <Link component="button" onClick={() => setShowBookmarklet(!showBookmarklet)}>
               ブックマークレットを表示
             </Link>
             <Collapse in={showBookmarklet}>
-              <Typography color="text.secondary" sx={{ mt: 1 }}>
-                ※ブックマークレットとは、ブラウザのブックマークに登録して使う小さなプログラムです。
-                以下の「コピー」ボタンでコピーしたコードをURLにしたブックマークを作成してください。
-                その後、IIDX公式サイトでブックマークをクリックすることで実行されます。
-              </Typography>
-              <Typography color="text.secondary" sx={{ mt: 1 }}>
-                取得されるのはチケットの情報のみで、外部サーバーに送信されることはありません。
-                取得したデータはブラウザのストレージにのみ保存され、 実際に読み込まれる
-                <Link href={import.meta.env.VITE_BOOKMARKLET_URL} target="_blank" rel="noopener noreferrer">
-                  スクリプト本体
-                </Link>
-                の内容は、公開されており、誰でも確認できます。
-              </Typography>
               <Box sx={{ mt: 1, border: "1px solid", borderColor: "divider" }}>
                 <Box sx={{ bgcolor: "action.hover", px: 2, py: 0.5 }}>
                   <Button size="small" startIcon={<ContentCopyIcon />} onClick={handleCopyToClipboard}>
@@ -101,6 +92,24 @@ export const TicketImporter: React.FC<TicketImporterProps> = ({ onImport }) => {
                 </Box>
                 <Box sx={{ p: 2, wordBreak: "break-all", bgcolor: "#f5f5f5" }}>{bookmarkletCode}</Box>
               </Box>
+              <Accordion sx={{ mt: 1 }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>補足</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography color="text.secondary" variant="body2">
+                    ※コピーしたコードをURLにしたブックマークを作成してください。
+                    その後、IIDX公式サイトでブックマークをクリックすることで実行されます。
+                    <br />
+                    取得されるのはチケットの情報のみで、外部サーバーに送信されることはありません。
+                    ソースコードはGitHubで公開されています。
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
             </Collapse>
           </StepContent>
         </Step>
