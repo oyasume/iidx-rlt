df464ed85204f21dff85ca46e4c7c1e854b4b1d1
iidx-rlt <215342013+oyasume@users.noreply.github.com>
2025-07-15 12:30:13 +0900

    style: モバイル版のタブ表示崩れを文字サイズ小さくして修正

--- src/components/layout/AppBottomNavigation.tsx
+++ src/components/layout/AppBottomNavigation.tsx
@@ -10,9 +10,20 @@ interface AppBottomNavigationProps {
 export const AppBottomNavigation: React.FC<AppBottomNavigationProps> = ({ navItems, tabIndex }) => {
   return (
     <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }} elevation={3}>
-      <BottomNavigation showLabels value={tabIndex}>
+      <BottomNavigation showLabels={false} value={tabIndex}>
         {navItems.map((tab) => (
-          <BottomNavigationAction key={tab.path} label={tab.label} icon={tab.icon} component={Link} to={tab.path} />
+          <BottomNavigationAction
+            key={tab.path}
+            label={tab.label}
+            icon={tab.icon}
+            component={Link}
+            to={tab.path}
+            sx={{
+              "& .MuiBottomNavigationAction-label": {
+                fontSize: "0.6rem",
+              },
+            }}
+          />
         ))}
       </BottomNavigation>
     </Paper>
