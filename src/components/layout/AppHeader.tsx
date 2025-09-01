8bcfbbf05d49b0d5ccec46a524018b17dedc1cbd
oyasume <215342013+oyasume@users.noreply.github.com>
2025-09-01 23:07:47 +0900

    feat: フィードバック催促の削除

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -1,54 +1,22 @@
-import CloseIcon from "@mui/icons-material/Close";
-import {
-  Alert,
-  alpha,
-  AppBar,
-  Button,
-  Collapse,
-  IconButton,
-  Link,
-  Switch,
-  Toolbar,
-  Typography,
-  useTheme,
-} from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { alpha, AppBar, Button, Link, Switch, Toolbar, Typography, useTheme } from "@mui/material";
+import React from "react";
 import ReactGA from "react-ga4";
 
 import { useSettingsStore } from "../../store/settingsStore";
 
 const APP_TITLE = "RLT Manager";
 const SURVEY_URL = "https://forms.gle/8PTuYZgbyFJwpEgu9";
-const SURVEY_NOTIFICATION_KEY = "surveyNotificationDismissed";
 
-const handleSurveyClick = (source: "Header" | "Alert") => {
+const handleSurveyClick = () => {
   ReactGA.event({
     category: "UserAction",
-    action: `Click Google Form (${source})`,
+    action: `Click Google Form (Header)`,
   });
 };
 
 export const AppHeader: React.FC = () => {
-  const [surveyAlertOpen, setSurveyAlertOpen] = useState(false);
   const toggleThemeMode = useSettingsStore((s) => s.toggleThemeMode);
   const theme = useTheme();
-
-  useEffect(() => {
-    const dismissed = localStorage.getItem(SURVEY_NOTIFICATION_KEY);
-    if (!dismissed) {
-      setSurveyAlertOpen(true);
-    }
-  }, []);
-
-  const handleCloseSurveyAlert = () => {
-    localStorage.setItem(SURVEY_NOTIFICATION_KEY, "true");
-    setSurveyAlertOpen(false);
-  };
-
-  const handleAlertSurveyClick = () => {
-    handleSurveyClick("Alert");
-    handleCloseSurveyAlert();
-  };
 
   return (
     <AppBar
@@ -70,37 +38,12 @@ export const AppHeader: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
           color="inherit"
-          onClick={() => handleSurveyClick("Header")}
+          onClick={handleSurveyClick}
         >
           フィードバック
         </Button>
         <Switch checked={theme.palette.mode === "dark"} onChange={toggleThemeMode} />
       </Toolbar>
-      <Collapse in={surveyAlertOpen}>
-        <Alert
-          severity="info"
-          action={
-            <>
-              <Button
-                color="inherit"
-                size="small"
-                href={SURVEY_URL}
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={handleAlertSurveyClick}
-              >
-                回答する
-              </Button>
-              <IconButton aria-label="close" color="inherit" size="small" onClick={handleCloseSurveyAlert}>
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            </>
-          }
-          sx={{ m: 2, mt: 0 }}
-        >
-          今後の開発のため、ご意見・改善案などを募集しています。
-        </Alert>
-      </Collapse>
     </AppBar>
   );
 };
