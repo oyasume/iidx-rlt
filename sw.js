02c011b534ae6d8020e256f4b4c0817ba6f90f90
monadnadnad <55864383+monadnadnad@users.noreply.github.com>
2025-09-28 01:54:03 +0900

    自爆 sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", async () => {
+  const keys = await caches.keys();
+  await Promise.all(keys.map(k => caches.delete(k)));
+  await self.registration.unregister();
+});
